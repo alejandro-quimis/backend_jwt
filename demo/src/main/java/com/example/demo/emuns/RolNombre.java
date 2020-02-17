1da9290532b42fbd5f2aa0168c50cf5bfe8141df
alejandro quimis <alejandro.quimisv@ug.edu.ec>
2020-02-17 14:51:34 -0500

    delete

--- demo/src/main/java/com/example/demo/emuns/RolNombre.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.emuns;
-
-public enum RolNombre {
-    ROLE_ADMIN,
-    ROLE_USER
-}
--- demo/src/main/java/com/example/demo/repository/ProductoR.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.models.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoR extends JpaRepository<Producto,Integer> {
-}
